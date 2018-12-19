a21222759320226e62cae5b855f7d860925df2c9
Indigoichigo <silentice1534@gmail.com>
2018-12-19 17:03:11 +0800

    1219 1702

--- 103_11_react.js/UDEMY_react/webpack.config.js
+++ 103_11_react.js/UDEMY_react/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/expensify-app/playground/redux-expensify.js',
+    entry: './src/indecision-app/app.js',
     output: {
         path: path.join(__dirname,'dist'),
         filename: 'bundle.js'
